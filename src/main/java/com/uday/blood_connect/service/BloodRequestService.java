19210dd231dd2487f2dc13a6ee3b153a5b2546e6
udayraj10 <cudayabi44@gmail.com>
2026-06-26 13:12:00 +0530

    added postgres dependency

--- src/main/java/com/uday/blood_connect/service/BloodRequestService.java
+++ src/main/java/com/uday/blood_connect/service/BloodRequestService.java
@@ -13,7 +13,6 @@ import com.uday.blood_connect.repository.DonationOfferRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
