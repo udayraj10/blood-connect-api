e64c7920b3589a04faf93ac6519571765011a2a0
udayraj10 <cudayabi44@gmail.com>
2026-07-21 16:38:03 +0530

    feat(exception): add ResourceEmptyException for empty data

---
+++ src/main/java/com/uday/blood_connect/exception/ResourceEmptyException.java
@@ -0,0 +1,8 @@
+package com.uday.blood_connect.exception;
+
+public class ResourceEmptyException extends RuntimeException {
+
+    public ResourceEmptyException(String message) {
+        super(message);
+    }
+}
