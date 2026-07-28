435246497cc509b4478d6ff3b96631fa3d242719
udayraj10 <cudayabi44@gmail.com>
2026-07-28 14:17:36 +0530

    refactor(config): replace CORS to vercel from github

--- src/main/java/com/uday/blood_connect/config/SecurityConfig.java
+++ src/main/java/com/uday/blood_connect/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -41,7 +42,7 @@ public class SecurityConfig {
                         cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers
-                        .frameOptions(frame -> frame.sameOrigin()))
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(auth -> auth
                         // Public Swagger/OpenAPI endpoints
                         .requestMatchers(
